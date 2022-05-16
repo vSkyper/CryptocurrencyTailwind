13649ff4c8a8be66fe916c9499c38580c6a3afd3
Mateusz Piwowarski <skyperpl@gmail.com>
2022-05-16 17:54:20 +0200

    Cards

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -67,35 +67,64 @@ const Main: React.FC = () => {
     'en-US'
   );
 
-  if (Object.keys(data).length != 0) {
+  if (Object.keys(data).length !== 0) {
     return (
       <main className='m-4 sm:container sm:mx-auto'>
-        <article className='prose dark:prose-invert md:prose-lg lg:prose-xl max-w-none'>
-          <h4>Cryptocurrency Prices by Market Cap</h4>
-          <h5>
-            The global cryptocurrency market cap today is {marketCapText}{' '}
-            Trillion, a{' '}
-            <span
-              className={
-                data.market_cap_change_percentage_24h_usd < 0
-                  ? 'text-red-500'
-                  : 'text-green-500'
-              }
-            >
-              {marketCapPercentage}
-            </span>{' '}
-            change in the last 24 hours. Total cryptocurrency trading volume in
-            the last day is at {totalVolumeText} Billion. Bitcoin dominance is
-            at {marketCapPercentageBTC} and Ethereum dominance is at{' '}
-            {marketCapPercentageETH}. CoinGecko API is now tracking{' '}
-            {cryptocurrencies} cryptocurrencies.
-          </h5>
-          <label className='form-switch'>
-            <input type='checkbox' />
-            <i></i>
-            <h5>Show stats</h5>
-          </label>
-        </article>
+        <p className='mb-2 text-xl sm:text-2xl font-semibold tracking-tight leading-loose dark:text-white'>
+          Cryptocurrency Prices by Market Cap
+        </p>
+        <p className='text-lg sm:text-xl font-normal tracking-tight dark:text-white'>
+          The global cryptocurrency market cap today is {marketCapText}{' '}
+          Trillion, a{' '}
+          <span
+            className={
+              data.market_cap_change_percentage_24h_usd < 0
+                ? 'text-red-500'
+                : 'text-green-500'
+            }
+          >
+            {marketCapPercentage}
+          </span>{' '}
+          change in the last 24 hours. Total cryptocurrency trading volume in
+          the last day is at {totalVolumeText} Billion. Bitcoin dominance is at{' '}
+          {marketCapPercentageBTC} and Ethereum dominance is at{' '}
+          {marketCapPercentageETH}. CoinGecko API is now tracking{' '}
+          {cryptocurrencies} cryptocurrencies.
+        </p>
+        <div className='mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4'>
+          <div className='p-4 sm:p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+            <h5 className='mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+              Market Capitalization
+            </h5>
+            <p className='text-lg font-normal text-gray-700 dark:text-gray-400'>
+              {marketCap}
+            </p>
+          </div>
+          <div className='p-4 sm:p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+            <h5 className='mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+              24h Trading Volume
+            </h5>
+            <p className='text-lg font-normal text-gray-700 dark:text-gray-400'>
+              {totalVolume}
+            </p>
+          </div>
+          <div className='p-4 sm:p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+            <h5 className='mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+              Bitcoin Market Cap Dominance
+            </h5>
+            <p className='text-lg font-normal text-gray-700 dark:text-gray-400'>
+              {marketCapPercentageBTC}
+            </p>
+          </div>
+          <div className='p-4 sm:p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+            <h5 className='mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+              # of Coins
+            </h5>
+            <p className='text-lg font-normal text-gray-700 dark:text-gray-400'>
+              {cryptocurrencies}
+            </p>
+          </div>
+        </div>
       </main>
     );
   }
