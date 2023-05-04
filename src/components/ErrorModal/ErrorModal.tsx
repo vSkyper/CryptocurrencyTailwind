8fcbe589b461bc506fb31494109c29e6b4b97a77
Mateusz Piwowarski <matpiw1@st.amu.edu.pl>
2023-05-04 13:27:08 +0200

    Change Error and Loading Modal

--- src/components/ErrorModal/ErrorModal.tsx
+++ src/components/ErrorModal/ErrorModal.tsx
@@ -1,8 +1,10 @@
 export default function ErrorModal() {
   return (
-    <div className='flex items-center justify-center'>
-      <div className='text-center p-5 rounded-xl bg-red-400 dark:bg-red-900 text-red-950 dark:text-red-50'>
-        Something went wrong
+    <div className='fixed h-full w-full top-0 left-0 bg-white bg-opacity-70 dark:bg-gray-800 dark:bg-opacity-80 flex items-center justify-center'>
+      <div className='flex items-center justify-center'>
+        <div className='text-center p-5 rounded-xl bg-red-500 dark:bg-red-900 text-red-50'>
+          Something went wrong
+        </div>
       </div>
     </div>
   )
